b6fff0d96496dea99282f3d6ee20aff88446d9be
RogerSellin <sellinroger@gmail.com>
2020-12-12 15:13:33 +0100

    UPDATE pdfconvert.py first argument cn now be just a name for outputfile

--- pdfconvert.py
+++ pdfconvert.py
@@ -1,14 +1,22 @@
 import sys
 from PIL import Image
+from os import path
 
 
 
 if len(sys.argv)<2:
+
     print("error not enough args")
     sys.exit()        
 
+
+first_file=1
+
+if not path.exists(sys.argv[1]): # to determine if the first argument is a file or just the outputname
+    first_file = 2
+
 pdflist=[]
-for arg in sys.argv[1:]:
+for arg in sys.argv[first_file:]:
         img = Image.open( arg)
         imgc = img.convert('RGB')
         pdflist.append(imgc)
